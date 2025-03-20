992b002420a9a419d7761b8157d8592680a1d1da
lily <lily.you@student.tdsb.on.ca>
2025-03-20 17:04:54 +0000

    Completed 2.2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,16 +3,63 @@ import java.util.Arrays;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-		int seqCount = compareSearch(new int[]{1,7,8,9,5}, 9);
-		System.out.println(seqCount);
 	}
 	
-	public static int compareSearch(int[] array, int number){
+	public static int[] compareSearch(int[] array, int number){
 		Arrays.sort(array);
 		int seqCount = 0;
-		while(array[seqCount] != number && seqCount < array.length-1){
+		for(int i = 0; i < array.length; i++){
 			seqCount++;
+			if(array[i] == number){
+				i = array.length;
+			}
 		}
-		return seqCount;
+		int mid;
+		int low = 0;
+		int binCount = 0;
+		int high = array.length - 1;
+		while(low <= high){
+			binCount++;
+			mid = (high + low)/2;
+			if(number == array[mid]){
+				low = high + 1;
+			}
+			else if(number > array[mid]){
+				low = mid + 1;
+			}
+			else{
+				high = mid -1;
+			}
+		}
+		return new int[]{seqCount, binCount};
 	}
+	public static int[] compareStringSearch(String[] array, String word){
+		Arrays.sort(array);
+		int seqCount = 0;
+		for(int i = 0; i < array.length; i++){
+			seqCount++;
+			if(array[i].equals(word)){
+				i = array.length;
+			}
+		}
+		int mid;
+		int low = 0;
+		int binCount = 0;
+		int high = array.length - 1;
+		while(low <= high){
+			binCount ++;
+			mid = (high + low)/2;
+			if(array[mid].equals(word)){
+				low = high + 1;
+			}
+			else if(word.compareTo(array[mid]) > 0){
+				low = mid + 1;
+			}
+			else{
+				high = mid -1;
+			}
+		}
+		return new int[]{seqCount, binCount};
+	}
+
 }
