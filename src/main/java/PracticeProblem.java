6efb890a780af93cc698a251d2b9718d0659facc
lily <lily.you@student.tdsb.on.ca>
2025-03-20 07:53:05 +0000

    m

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,18 @@
+import java.util.Arrays;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-		
+		int seqCount = compareSearch(new int[]{1,7,8,9,5}, 9);
+		System.out.println(seqCount);
 	}
 	
+	public static int compareSearch(int[] array, int number){
+		Arrays.sort(array);
+		int seqCount = 0;
+		while(array[seqCount] != number && seqCount < array.length-1){
+			seqCount++;
+		}
+		return seqCount;
+	}
 }
